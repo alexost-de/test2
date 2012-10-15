3581ad608ca8e4bf20353885156590d18a9d1dfd
admin <admin@192.168.1.2>
2012-10-15 23:30:14 +0200

    Message in Index-Controller und Index-View in Master

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,10 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        
+    	$this->view->message = 'Hallo World!';
+    	
+    
     }
 
 
